179c6c40bfcb496c2c4674550fa2dbb4ab6e48bf
Anthony Blake <anthonix@me.com>
2012-10-06 16:22:45 +1300

    Fixed copyright

--- tb/sr_half_odd_sequence_tb.vhdl
+++ tb/sr_half_odd_sequence_tb.vhdl
@@ -1,27 +1,34 @@
--------------------------------------------------------------------------------
--- Title      : Testbench for design "sr_half_odd_sequence"
--- Project    : NTP S-1 Appliance
--------------------------------------------------------------------------------
--- File       : sr_half_odd_sequence_tb.vhdl
--- Author     :   <a@anthonix.com>
--- Company    : 
--- Created    : 2010-12-28
--- Last update: 2011-01-10
--- Platform   : 
--- Standard   : VHDL'87
--------------------------------------------------------------------------------
--- This file is part of the NTP S-1 appliance project. For more information 
--- see http://www.wand.net.nz/~amb33/ntp
--------------------------------------------------------------------------------
--- Description: 
--------------------------------------------------------------------------------
--- Copyright (c) 2010 
--------------------------------------------------------------------------------
--- Revisions  :
--- Date        Version  Author  Description
--- 2010-12-28  1.0      anthonix	Created
--------------------------------------------------------------------------------
-
+--                                    
+--                              COPYRIGHT                             
+--  
+--  High-performance FFT library 
+--  Copyright (c) 2010, The University of Waikato, New Zealand
+--  All rights reserved.
+--  
+--  Author: Anthony Blake (a@anthonix.com)
+--  
+--  Redistribution and use in source and binary forms, with or without
+--  modification, are permitted provided that the following conditions are met:
+--      * Redistributions of source code must retain the above copyright
+--        notice, this list of conditions and the following disclaimer.
+--      * Redistributions in binary form must reproduce the above copyright
+--        notice, this list of conditions and the following disclaimer in the
+--        documentation and/or other materials provided with the distribution.
+--      * Neither the name of the organization nor the
+--        names of its contributors may be used to endorse or promote products
+--        derived from this software without specific prior written permission.
+--  
+--  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+--  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+--  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+--  DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF WAIKATO BE LIABLE FOR ANY
+--  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+--  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+--  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+--  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+--  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+--  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
 library ieee;
 use ieee.std_logic_1164.all;
 
